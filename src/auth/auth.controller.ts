9e89df9fe8951122f4acc806974f94aed685720d
Sasha <sashayatsikovich@gmail.com>
2024-01-26 17:04:01 +0200

    add basic modules with auth + basic e2e setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
+import { log } from 'console';
+import { AuthDto } from './dto';
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  singup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  singin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
 }
